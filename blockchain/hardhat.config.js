eba5df53f3567a00ad0a16c2e3662ae04f3dfe08
bidyadharsahu <131295002+bidyadharsahu@users.noreply.github.com>
2024-09-21 14:41:28 +0530

    hardhat.config.js

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,7 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
-  solidity: "0.8.21",
+  solidity: "0.8.27",
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/jlSgEfhAbhIIBXS8YCV7G4fjaYCSRAH2`,  // Replace with your Alchemy Sepolia URL
