d9bf5ddd33adbb4e125f2760071975f446abbd65
bidyadharsahu <131295002+bidyadharsahu@users.noreply.github.com>
2024-09-22 19:30:44 +0530

    Add files via upload

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,11 +1,12 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomicfoundation/hardhat-toolbox");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/jlSgEfhAbhIIBXS8YCV7G4fjaYCSRAH2`,  // Replace with your Alchemy Sepolia URL
-      accounts: [`0x${34480e9bd7324c0ecd786ed878e0236bced228443fb63e0336a4bba90f867d31}`]  // Replace with your private key
+      accounts: `remote`  // Replace with your private key
     }
   }
 };
